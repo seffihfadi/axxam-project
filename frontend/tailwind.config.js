806aa14a5811884ed6ef945ca8de746026c746fe
Seffih Fadi <seffihfadi2004@gmail.com>
2024-02-28 12:27:57 +0100

    add some colors to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,14 @@ export default {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#00A472',
+        secondary: '#6D6D6D',
+        whitemode: '#F7F7F7',
+        darkmode: '#1E1E1E'
+      }
+    },
   },
   plugins: [],
 }
