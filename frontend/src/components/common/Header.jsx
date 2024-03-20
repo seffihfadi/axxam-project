250c6ead2eb1024dcb6faf148bc1f2a0cdfe44f8
Seffih Fadi <seffihfadi2004@gmail.com>
2024-03-20 13:28:36 +0100

    solve header and footer problems

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -3,45 +3,29 @@ import { useEffect } from "react";
 import { useRef } from "react";
 const Header = () => {
   const headerRef = useRef();
-  useEffect( () =>{
-window.addEventListener("scroll" , () => {
-  if ( window.scrollY > 100 ){
-headerRef.current.style.background = " #EEEEEE" ;
-headerRef.current.style.padding  = " 20px 20px "
-  }else {
-    headerRef.current.style.background = " #F7F7F7" ;
-    headerRef.current.style.padding  = " 0px 20px"
-  }})})
+//   useEffect( () =>{
+// window.addEventListener("scroll" , () => {
+//   if ( window.scrollY > 100 ){
+// headerRef.current.style.background = " #EEEEEE" ;
+// headerRef.current.style.padding  = " 20px 20px "
+//   }else {
+//     headerRef.current.style.background = " #F7F7F7" ;
+//     headerRef.current.style.padding  = " 0px 20px"
+//   }})})
   return (
-    <section>
-    <div  ref={headerRef}  className ="  pb-[60px] pt-[60px] fixed z-10000000 w-full  lef-0 top-0 transition-all duration-200   "> 
-      <div className=" px-[20px] mx-auto   flex justify-between items-center gap-[20px] lg:gap-[40px]  md:gap-[10px] flex-col md:flex-row  ">
-      <a href="/"> <img src=" /src/assets/logo.svg "  alt="logo-img" className=" object-contain w-[112,99px] h-[48.59px]  md:mr-3 lg:mr-0 "/> </a>
-      <nav className="flex items-center gap-[20px] lg:gap-[70px] md:gap-[10px] flex-col md:flex-row">
-      <ul className="flex  items-center  font-semibold  h-[30px] gap-[15px] lg:gap-[25px] md:gap-[10px]  md:max-w-full     ">
-            <li className="brd"><a href="/ Home"> Home </a></li>   
-            <li className="brd"><a href="/Our services"> Our services </a></li>
-            <li className="brd "><a href="/Properties">Properties </a></li>
-            <li className="brd"><a href="/Contact Us">Contact Us  </a></li>   
-            <li className="brd"><a href="/">  </a></li>   
-        </ul>
-        <ul className=" flex items-center  gap-[15px] md:max-w-full md:gap-[15px] ">
-            <li>
-            <a href="/" className="btn w-[130px] h-[40px] md:w-[100px] md:h-[35px]  lg:w-[130px] lg:h-[40px] text-[#0051CB] font-medium  element-center rounded-[10px] ">
-                   SIGN UP   
-                  </a>
-            </li>
-            <li>
-            <a href="/" className="btn w-[130px] h-[40px] md:w-[100px] md:h-[35px]  lg:w-[130px] lg:h-[40px]  text-white bg-[#0051CB] element-center font-medium  rounded-[10px] ">
-                   JOIN US  
-                  </a>
-            </li>
-        </ul>
-
+    <header ref={headerRef} className ="px-6 z-50 bg-white shadow-lg py-3 fixed w-full left-0 flex justify-between items-center top-0"> 
+      <img src=" /src/assets/logo.svg" alt="logo-img" className="cursor-pointer object-contain h-8"/>
+      <nav className="justify-center items-center gap-5 hidden md:flex">
+        <a href="/Home"> Home </a>
+        <a href="/Ourservices"> Our services </a>
+        <a href="/Properties">Properties </a>
+        <a href="/ContactUs">Contact Us</a>
       </nav>
+      <div className="flex gap-3">
+        <button className="secondary-sq">Join us</button>
+        <button className="primary">Signup</button>
       </div>
-    </div>
-    </section>
+    </header>
   )
 }
 
