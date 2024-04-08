0b847e4630fe8ff88852de676102e9779520b4c8
AminaChe <a_chelighem@estin.dz>
2024-04-08 19:52:06 +0100

    add Notification.jsx UserGreeting.jsx sectionthree.jsx and modify Header index.css

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -19,8 +19,8 @@ class Header extends Component {
         <div className="  py-3 px-0 mx-0 fixed z-50 w-full shadow-lg bg-white dark:bg-darkmode left-0 top-0">
           <div className="px-6 mx-auto flex justify-between items-center gap-[3px] lg:gap-[40px] md:gap-[5px] flex-row">
             <a href="/"><img src="/src/assets/logo.svg" alt="logo-img" className="object-contain w-20 md:mr-3 lg:mr-0" /></a>
-            <nav className="flex items-center gap-[5px] lg:gap-[150px] md:gap-[18px] sm:flex-row">
-              <div   className={ this.state.clicked  ? "navbar active w-full  items-center font-semibold bg-white dark:bg-darkmode  h-[30px] lg:gap-[35px] md:gap-[15px]  " : "navbar  items-center font-semibold h-[30px] lg:gap-[35px] md:gap-[15px] "} >
+            <nav className="flex items-center gap-[5px] lg:gap-[150px] md:gap-[20px] sm:flex-row">
+              <div   className={ this.state.clicked  ? "navbar active w-full  items-center font-semibold bg-white dark:bg-darkmode  h-[30px] lg:gap-[35px] md:gap-[10px]  " : "navbar  items-center font-semibold h-[30px] lg:gap-[35px] md:gap-[10px] "} >
                 <a href="/Home" className="brd">Home</a>
                 <a href="/Our services" className="brd">Our services</a>
                 <a href="/Properties" className="brd">Properties</a>
