35ab324cfdcb77a4b94b19b22f650d7f39a89c19
Saighi Achraf <achraf@Saighis-MacBook-Pro.local>
2024-06-04 23:50:29 +0100

    linked user profile

--- frontend/src/components/MainInformations/Form.jsx
+++ frontend/src/components/MainInformations/Form.jsx
@@ -2,20 +2,12 @@ import React, { useState } from "react";
 import Image from "../../../public/bg4.jpg";
 import { HiOutlineCamera } from "react-icons/hi2";
 import { useDropzone } from "react-dropzone";
-function Form() {
-  const userinfo = {
-    FullName: "Bouzidi Ahmad",
-    PhoneNumber: "+213 559 65 21 95",
-    Birthdate: "22/05/2001",
-    Gender: "Male",
-    Biography: "Not provided",
-    Address: "Not provided",
-    image: Image,
-  };
-  const birthdateParts = userinfo.Birthdate.split("/");
-  const formattedBirthdate = `${birthdateParts[2]}-${birthdateParts[1]}-${birthdateParts[0]}`;
-  const [image, setImage] = useState(userinfo.image); // State for the image
+import { useGetUserQuery } from "../../features/user/userApiSlice";
 
+function Form() {
+  const {data: user, isLoading: userIsLoading} = useGetUserQuery()
+  const [image, setImage] = useState(user.user.avatar); // State for the image
+ console.log(user)
   const { getRootProps, getInputProps } = useDropzone({
     accept: "image/*", // Only accept image files
     onDrop: (acceptedFiles) => {
@@ -32,45 +24,37 @@ function Form() {
             <h1 className=" md:pt-9 md:pb-18 pb-12 mx-auto md:mx-0 text-center md:text-left font-bold text-xl ">Main Informations</h1>
             
               <div className="main">
-                <input id="Fullname" name="Fullname" type="text" placeholder={userinfo.FullName} required/>
+                <input id="Fullname" name="Fullname" type="text" defaultValue={user.user.fullname} required/>
                 <label htmlFor="Fullname">Full name</label>
               </div>
               <div className="main">
-                <input id="phonenumber" name="phonenumber" type="tel" placeholder={userinfo.PhoneNumber}  required/>
+                <input id="phonenumber" name="phonenumber" type="tel" value={user.user.phone}  readOnly/>
                 <label htmlFor="Fullname">Phone number</label>
               </div>
               <div className="main ">
-                <input id="birthdate" name="birthdate" type="date" placeholder={formattedBirthdate} required  />
+                <input id="birthdate" name="birthdate" type="text" placeholder={user.user.birthDate.slice(0,10)} readOnly  />
                 <label htmlFor="birthdate">Birthdate</label>
               </div>
-           
             <button className="primary info">Save</button>
           </div>
           <div className='pb-32'>
             <h1 className="pb-10 pt-3 md:pb-18 mx-auto md:mx-0 text-center md:text-left font-bold text-xl ">Additional informations</h1>
             <div className="main ">
-                <select  id="sdf">
-                  <option  className="darktxt dark:bg-darkmode"  placeholder={userinfo.Gender === "Male"} >
-                    Male
-                  </option>
-                  <option className="darktxt dark:bg-darkmode" placeholder={userinfo.Gender === "Female"} >
-                    Female
-                  </option>
-                </select>
+                <input id="gender" name="gender" type="text" value={user.user.extra.gender} readOnly/>
                 <label htmlFor="sdf">Gender</label>
               </div>
               <div className="main">
-                <input id="Biography" name="Biography" type="text" placeholder={userinfo.Biography} required />
+                <input id="Biography" name="Biography" type="text" defaultValue={user.user.extra.bio} required />
                 <label htmlFor="Biography">Biography</label>
               </div>
               <div className="main">
-                <input id="Address"name="Address" type="text" placeholder={userinfo.Biography} required />
+                <input id="Address"name="Address" type="text" defaultValue={`${user.user.extra.livesIn.charAt(0).toUpperCase()}${user.user.extra.livesIn.slice(1)}`} required />
                 <label htmlFor="Address">Address</label>
               </div>
             
             <button className='primary info'>Save</button>
-         </div>
-         </form>
+        </div>
+        </form>
         
         <div className=" w-full pb-14 md:mt-32 mt-12 flex justify-center  ">
           <div {...getRootProps({ className:"sticky top-20 lg:w-44 lg:h-44 md:w-36 md:h-36 w-28 h-28 flex relative", })}>
@@ -85,7 +69,7 @@ function Form() {
             </button>
           </div>
         </div>
-       
+      
       </div>
     </div>
   );
---
+++ frontend/src/features/user/userApiSlice.js
@@ -0,0 +1,17 @@
+import { apiSlice } from "../../api/apiSlice"
+
+export const useApiSlice = apiSlice.injectEndpoints({
+  endpoints: builder => ({
+    getUser : builder.query({
+      query: () => '/get-user',
+    }),
+    getUserId: builder.query({
+      query: (userID) => `/get-user-by-id/:${userID}`
+    })
+  })
+})
+
+export const {
+  useGetUserQuery,
+  useGetUserIdQuery,
+} = useApiSlice
